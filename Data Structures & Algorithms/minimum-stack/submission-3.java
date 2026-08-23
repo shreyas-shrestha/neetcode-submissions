bb21ffb9ef671683ad8c134836cad218c81b61f8
Shreyas Shrestha <59338556+shreyas-shrestha@users.noreply.github.com>
2026-08-22 22:31:16 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,36 @@
+class MinStack {
+    Deque<Integer> myList;
+    int minVal;
+
+    public MinStack() {
+        myList = new ArrayDeque();
+        minVal = Integer.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        myList.addFirst(val);
+        if (val < minVal) {
+            minVal = val;
+        }
+    }
+    
+    public void pop() {
+        int removed = myList.removeFirst();
+        if (removed == minVal) {
+            minVal = Integer.MAX_VALUE;
+            for (int i : myList) {
+                if (i < minVal) {
+                    minVal = i;
+                }
+            }
+        }
+    }
+    
+    public int top() {
+        return myList.peekFirst();
+    }
+    
+    public int getMin() {
+        return minVal;
+    }
+}
